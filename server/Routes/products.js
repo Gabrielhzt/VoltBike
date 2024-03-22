e00d37a98b70dc7fe2641a7af300807dafdca788
Gabrielhzt <hazout26@gmail.com>
2024-03-22 06:46:59 +0100

    Some changes in product route

--- server/Routes/products.js
+++ server/Routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../database');
 
 router.get('/', (req, res) => {
-    pool.query('SELECT name, price FROM products', (error, result) => {
+    pool.query('SELECT product_id,name, price, image_url FROM products', (error, result) => {
         if (error) {
             console.error('Error retrieving products:', error);
             res.status(500).send('Error retrieving products');
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 router.get('/:productId', (req, res) => {
     const { productId } = req.params;
 
-    pool.query('SELECT name, description, price FROM products WHERE product_id = $1', [productId], (error, result) => {
+    pool.query('SELECT name, description, price, image_1 FROM products WHERE product_id = $1', [productId], (error, result) => {
         if (error) {
             console.error('Error retrieving product:', error);
             res.status(500).send('Error retrieving product');
